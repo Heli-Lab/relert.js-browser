81acc1bc52ffde5ad4806c3a0b476e917e0163ac
Heli-Lab <heli_lab@foxmail.com>
2021-08-26 16:32:28 +0800

    register基类功能完善

--- script/relert.register.js
+++ script/relert.register.js
@@ -7,7 +7,7 @@
     this.entrance = '';
     this.register = '';
     this.defaults = {};
-    this.exports = ['parameters', 'entrance', 'forEach', 'add', 'delete', 'empty', Symbol.iterator];
+    this.exports = ['parameters', 'entrance', 'forEach', 'add', 'delete', 'empty', 'includes', Symbol.iterator];
     this.parent = undefined;
 
     this.checkArray = () => {
@@ -27,9 +27,9 @@
         },
         get: (obj, key) => {
             this.checkArray();
-            if (this.exports.indexOf(key) >= 0) {
+            if (this.exports.includes(key)) {
                 return obj[key];
-            } else if (['length', 'count'].indexOf(key) >= 0) {
+            } else if (['length', 'count'].includes(key)) {
                 if (this.arrayLike) {
                     return obj.parent.INI[obj.register].length;
                 } else {
@@ -37,6 +37,8 @@
                 }
             } else if (obj.parent.INI[obj.register][key]) {
                 return obj.getInterface(key);
+            } else if (obj.parent.INI[obj.register].includes(key)) {
+                return obj.getInterface(obj.parent.INI[obj.register].indexOf(key));
             }
         },
         deleteProperty: (obj, key) => {
@@ -70,22 +72,87 @@
     }
 
     this.getInterface = (index) => {
-        return this.parent.INI[this.parent.INI[this.register][index]];
+        return {
+            Name: this.parent.INI[this.register][index],
+            INI: this.parent.INI[this.parent.INI[this.register][index]],
+        };
     }
+
+    // forEach迭代器
+    this.forEach = (callback) => {
+        let lastItem = null;
+        for (index in this.parent.INI[this.register]) {
+            do {
+                lastItem = this.parent.INI[this.register][index];
+                callback(this.getInterface(index), index);
+            } while (!(lastItem == this.parent.INI[this.register][index]));
+        }
+    }
+
+    // for ... of Iterator迭代器
+    this[Symbol.iterator] = function*() {
+        let lastItem = null;
+        for (index in this.parent.INI[this.register]) {
+            do {
+                lastItem = this.parent.INI[this.register][index];
+                yield this.getInterface(index);
+            } while (!(lastItem == this.parent.INI[this.register][index]));
+        }
+    }.bind(this);
 
     // 新增
     this.add = (obj) => {
-        // Todo
+        if (!obj.Name) {
+            throw new Error(`relert.${this.entrance}.add Error - Parameter not found: Name`);
+        }
+        let index = this.parent.INI[this.register].push(obj.Name);
+        if (!this.parent.INI[obj.Name]) {
+            this.parent.INI[obj.Name] = {};
+        }
+        for (let i in obj.INI) {
+            this.parent.INI[obj.Name][i] = obj.INI[i];
+        }
+        for (let i in this.defaults) {
+            if (!this.parent.INI[obj.Name][i]) {
+                this.parent.INI[obj.Name][i] = this.defaults[i];
+            }
+        }
+        return this.getInterface(index - 1);
+    }
+
+    // 判断是否有注册
+    this.includes = (key) => {
+        return obj.parent.INI[obj.register].includes(key);
     }
 
     // 批量删除
     this.delete = (judge) => {
-        // Todo
+        if (typeof judge == 'function') {
+            this.forEach((item, index) => {
+                if (judge(item)) {
+                    delete item.INI;
+                    this.parent.INI[this.register].splice(index, 1);
+                }
+            });
+        } else if (typeof judge == 'object') {
+            this.forEach((item, index) => {
+                for (let key in judge) {
+                    if (item[key] != judge[key]) {
+                        return;
+                    }
+                    delete item.INI;
+                    this.parent.INI[this.register].splice(index, 1);
+                }
+            });
+        }
     }
 
     // 置空
     this.empty = () => {
-        // Todo
+        this.forEach((item) => {
+            delete item.INI;
+        });
+        this.parent.INI[this.register] = [];
     }
 }
 
